376428c0c106ec3960f79256be8d452e89176ce1
Kouhei Sutou <kou@clear-code.com>
2014-07-13 16:22:14 +0900

    Fix escape

--- lib/test/unit/notify/version.rb
+++ lib/test/unit/notify/version.rb
@@ -2,7 +2,7 @@ module Test
   module Unit
     module Notify
       # test-unit-notify version number formatted as
-      # "#\{MAJOR}.#\{MINOR}.#\{MICRO}".
+      # "#\\{MAJOR}.#\\{MINOR}.#\\{MICRO}".
       VERSION = "1.0.2"
     end
   end
