af20fef399dc47cbdc604f8006b49ae5a6e0e184
Kouhei Sutou <kou@clear-code.com>
2014-08-03 14:46:09 +0900

    Bump version

--- lib/test/unit/notify/version.rb
+++ lib/test/unit/notify/version.rb
@@ -3,7 +3,7 @@ module Test
     module Notify
       # test-unit-notify version number formatted as
       # "#\\{MAJOR}.#\\{MINOR}.#\\{MICRO}".
-      VERSION = "1.0.3"
+      VERSION = "1.0.4"
     end
   end
 end
